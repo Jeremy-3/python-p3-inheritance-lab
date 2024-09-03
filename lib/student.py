d871e38b6c5269a3913745316b20727a6580aacb
+ <+>
2024-09-03 08:12:03 +0300

    test done

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,17 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+        
+    def learn(self,topic):
+        self.knowledge.append(topic)
+            
+   
+        
+            
     
-    def learn(self):
-        pass
+    
+    
+        
